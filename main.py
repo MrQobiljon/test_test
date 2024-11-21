03df8db21986688b90c9e221b1507c8ec5934623
Qobiljon <86218810+MrQobiljon@users.noreply.github.com>
2024-11-21 21:58:39 +0500

    Revert "test2"

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+def say_hello():
+    return "Hello World"
+
+def say_hello1():
+    return "Privet"
+
+def say_hello2():
+    return "Assalomu alaykum"
+
+def test0():
+    return "test0"
+
 def test1():
     return "test1"
-
-def test2():
-    return "test2"
